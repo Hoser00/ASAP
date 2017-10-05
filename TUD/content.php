6595527e2815f91075d28280204f9e8ad7cc6d3f
Mizar\JPDay <manwe@att.net>
2017-10-05 15:15:24 -0700

    Added Ron4TUD

---
+++ TUD/content.php
@@ -0,0 +1,27 @@
+<?php
+// Temp index file
+
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html>
+<head><title>RonforTUD</title>
+
+</head>
+<body>
+
+<h2>Rate-Payer Ron for TUD Director</h2>
+This is the content page.
+
+<ul>Here's why Ron is the right choice
+<li>this is item one</li>
+<li>this is itme two</li>
+</ul>
+
+<br clear="all" />
+
+</body>
+</html>
---
+++ TUD/index.php
@@ -0,0 +1,73 @@
+<?php
+// Temp index file
+
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html>
+<head><title>RonforTUD</title>
+<link rel="stylesheet" href="R4T.css" type="text/css" media="screen">
+
+</head>
+<body>
+<div id="hdrID" style="position:absolute;top:0px;left:0px;z-index:1;width:1500px;">
+<img src="images/R4T_hdr1.jpg" height="330" width="720" alt="" class="hdr" />
+<img src="images/R4T_hdr2.jpg" height="330" width="70" alt="" class="hdr" />
+<img src="images/R4T_hdr3.jpg" height="300" width="710" alt="" class="hdr" />
+</div>
+<div id="mainID" style="position:absolute;top:300px;left:0px;z-index:2;width:1500px;">
+
+<div id="sectOneID" style="float:left;width:720px;background-color:transparent;margin-top:30px;">
+
+<div style="width:700px;margin-left:20px;background-color:#87796f;">
+
+  <div class="contborder"><img src="images/blank.gif" height="430" width="1px" alt="" /></div>
+  <div id="ContWrapperID" style="float:left;width:678px;background-color:#fff;margin-bottom:10px;">
+
+    <div style="float:left;"><img src="images/blank.gif" height="420" width="1px" alt="" /></div>
+    <div id="contentID" style="border:solid 10px white;margin-bottom:10px;">
+
+     <iframe id="ifID" src="content.php" frameborder="0" scrolling="auto" height="400" width="650" ></iframe>
+
+    </div>
+    
+
+  </div>
+
+  <div class="contborder"><img src="images/blank.gif" height="430" width="1px" alt="" /></div><br clear="all" />
+</div>
+
+
+</div>
+
+<div id="spacerID" style="float:left;width:70px;margin-top:30px;background-color:gold;">MIDDLE</div>
+<div id="sectTwoID" style="float:left;width:710px;background-color:green;">
+
+<div style="width:700px;margin-left:10px;background-color:#87796f;">
+
+  <div class="contborder"><img src="images/blank.gif" height="430" width="1px" alt="" /></div>
+  <div id="ContWrapper2ID" style="float:left;width:678px;background-color:#fff;margin-bottom:10px;">
+
+    <div style="float:left;"><img src="images/blank.gif" height="420" width="1px" alt="" /></div>
+    <div id="content2ID" style="border:solid 10px white;margin-bottom:10px;">
+
+Section 2
+
+    </div>
+    
+
+  </div>
+
+  <div class="contborder"><img src="images/blank.gif" height="430" width="1px" alt="" /></div><br clear="all" />
+</div>
+
+</div>
+
+<br clear="all" />
+60% actual size
+
+</body>
+</html>
